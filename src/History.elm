4eaf14bddf0369cb0e6e5bff1165d8174a92affc
CMoncur <CMoncur@gmail.com>
2017-02-24 14:28:42 -0800

    Base types, pack and unpack

--- src/History.elm
+++ src/History.elm
@@ -1,8 +1,37 @@
 module History exposing
   ( History
+  , pack
+  , unpack
   -- , goBack
   -- , goBackNum
   )
 
-type History
-  = History ( String, a )
+{-|
+# Primitives
+@docs History
+
+# Utilities
+@docs unpack, pack
+-}
+
+{-| History item type, where `a` represents route and
+`b` represents a model or similar record to store.
+-}
+type History a
+  = History a
+
+{-| Subscribe to browser's onPopState event
+
+    unpack yeah == "yeah"
+-}
+pack : a -> ( History a )
+pack a =
+  History a
+
+{-| Subscribe to browser's onPopState event
+
+    unpack yeah == "yeah"
+-}
+unpack : ( History a ) -> a
+unpack ( History a ) =
+  a
