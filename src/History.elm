be0f7c15e920c4e2fe44a1f2a72c0090d68cd0ac
CMoncur <CMoncur@gmail.com>
2017-03-02 16:53:34 -0800

    Entry type and History.push

--- src/History.elm
+++ src/History.elm
@@ -1,37 +1,104 @@
 module History exposing
-  ( History
-  , pack
-  , unpack
-  -- , goBack
-  -- , goBackNum
+  ( Entry
+  , History
+  , back
+  , init
+  , push
+  , revise
   )
 
 {-|
 # Primitives
-@docs History
+@docs Entry, History
 
 # Utilities
-@docs unpack, pack
+@docs back, init, push, revise
 -}
+
+-- Core Dependencies
+import Navigation as Nav
+
+{-| Entry item type, where `a` represents route and
+`b` represents a model or similar record to store.
+-}
+type alias Entry a =
+  { model     : History a
+  , revisions : a
+  , url       : String
+  }
 
 {-| History item type, where `a` represents route and
 `b` represents a model or similar record to store.
 -}
-type History a
-  = History a
+type alias History a =
+  { current : a
+  , history : List a
+  }
 
 {-| Subscribe to browser's onPopState event
 
-    unpack yeah == "yeah"
+    pack yeah == "yeah"
 -}
-pack : a -> ( History a )
-pack a =
-  History a
+back : History a -> History a
+back model =
+  let
+    recent = Maybe.withDefault
+      model.current
+      ( List.head model.history )
+
+    remainder =
+      remaining model.history
+  in
+    { history = remainder
+    , current = recent
+    }
 
 {-| Subscribe to browser's onPopState event
 
-    unpack yeah == "yeah"
+    pack yeah == "yeah"
 -}
-unpack : ( History a ) -> a
-unpack ( History a ) =
-  a
+init : a -> History a
+init initial_state =
+  { history = []
+  , current = initial_state
+  }
+
+{-| Subscribe to browser's onPopState event
+
+    pack yeah == "yeah"
+-}
+push : Entry a -> ( History a, Cmd msg )
+push entry =
+  let
+    m = entry.model
+  in
+    { m
+    | history = m.current :: m.history
+    , current = entry.revisions
+    }
+    ! [ Nav.newUrl entry.url ]
+
+{-| Subscribe to browser's onPopState event
+
+    pack yeah == "yeah"
+-}
+revise : History a -> a -> History a
+revise model revisions =
+  { model | current = revisions }
+
+
+{-------------------------------}
+{--- Private Functions ---------}
+{-------------------------------}
+
+{-| Subscribe to browser's onPopState event
+
+    pack yeah == "yeah"
+-}
+remaining : List a -> List a
+remaining history =
+  case history of
+    []       -> []
+    [ a ]    -> []
+    [ a, b ] -> [ b ]
+    a :: b   -> b
---
+++ src/Main.elm
@@ -0,0 +1,115 @@
+module Main exposing ( main )
+
+-- Core Dependencies
+import Html exposing
+  ( Html
+  , br
+  , button
+  , div
+  , p
+  , text
+  )
+import Html.Events exposing ( onClick )
+
+-- Package Dependencies
+import History exposing ( History )
+import Navigation exposing ( Location )
+
+-- Model/Msg Types
+type alias Base =
+  { route : String
+  }
+
+type alias Model = History Base
+
+type Msg
+  = GoBack
+  | Navigate String
+  | UrlChange Location
+
+-- History Functions
+
+
+-- Update Function
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  let m = model.current in
+  case msg of
+    GoBack ->
+      let
+        fresh_model = History.back model
+      in
+        fresh_model
+        ! [ Navigation.modifyUrl fresh_model.current.route ]
+
+    Navigate url ->
+      History.push
+        { model     = model
+        , revisions = { m | route = url }
+        , url       = url
+        }
+
+    UrlChange _ ->
+      model ! [ Cmd.none ]
+
+-- View Functions
+buttons : List String
+buttons =
+  [ "/"
+  , "/about"
+  , "/contact"
+  , "/portfolio"
+  , "/transition"
+  , "/notfound"
+  ]
+
+renderButton : String -> Html Msg
+renderButton button_text =
+  button
+    [ onClick ( Navigate button_text ) ]
+    [ text <| String.dropLeft 1 button_text ]
+
+renderHistory : Model -> Html Msg
+renderHistory model =
+  div [] ( List.map renderHistoryItem model.history )
+
+renderHistoryItem : Base -> Html Msg
+renderHistoryItem m =
+  p [] [ text <| toString m ]
+
+view : Model -> Html Msg
+view model =
+  div
+    []
+    [ div
+        []
+        ( ( button
+            [ onClick GoBack ]
+            [ text "Go Back" ]
+        ) :: ( List.map renderButton buttons ) )
+    , br [] []
+    , renderHistory model
+    ]
+
+
+-- Init Function
+init : Location -> ( Model, Cmd Msg )
+init location =
+  ( History.init initModel
+  , Cmd.none
+  )
+
+initModel : Base
+initModel =
+  { route = "/"
+  }
+
+-- Elm Main
+main : Program Never Model Msg
+main =
+  Navigation.program UrlChange
+    { init          = init
+    , subscriptions = (\ _ -> Sub.none )
+    , update        = update
+    , view          = view
+    }
